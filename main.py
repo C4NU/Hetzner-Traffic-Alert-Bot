3601f55865f0e6e297a1986f0a128cc67e0b5158
C4NU <canu1832@gmail.com>
2022-08-18 00:32:14 +0900

    [ADD] Basic Functions Complete. [REQ] Fix for public release.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from sched import scheduler
 from hcloud import Client
 
 from hcloud.images.domain import Image
@@ -5,23 +6,86 @@ from hcloud.servers.domain import Server
 from hcloud.server_types.domain import ServerType
 from hcloud.volumes.domain import Volume
 
-import telegram
-import json 
+import urllib.request
+import schedule
+import json # json package
+import time
+import math
 
+import telegram # python-telegram-module package
+
+# Open Json file - use this section when clone project.
 #with open("keys.json") as f:
 #	keys_data = json.load(f)
 
 with open("my_keys.json") as f:
 	keys_data = json.load(f)
 
-# Create a client
-client = Client(token=keys_data["hetzner"]["token"])
+# Initialize Telegram Bot
+bot = telegram.Bot(token=keys_data["telegram"]["token"])
+chatID = keys_data["telegram"]["chatID"]
 
-boundServer = client.servers.get_all()
-print(boundServer)
+# Create a client - remove hashtag
+# client = Client(token=keys_data["hetzner"]["token"])
 
-server = boundServer[0]
-print(server.name)
-print("Outgoin Traffic: "+str(server.outgoing_traffic)) # 헤츠너 클라우드에서 뜨는 실제로 쓰고있는 트래픽
-print("Ingoing Traffic: "+str(server.ingoing_traffic)) # 들어오는 트래픽?
-print("included_traffic: "+str(server.included_traffic)) # 무료 트래픽
+client_1 = Client(token=keys_data["hetzner"]["canu1832_token"])
+client_2 = Client(token=keys_data["hetzner"]["wakeup_token"])
+client_3 = Client(token=keys_data["hetzner"]["phium_token"])
+
+def GetServer(client):
+	boundServer = client.servers.get_all() # Servers on List
+	server = boundServer[0] # Get first server from list
+
+	return server # return first server
+
+def ByteToTB(byteTraffic):
+	result = byteTraffic
+	for i in range(0, 4):
+		result = result / 1024
+	
+	string = "트래픽 사용량: "+str(round(result,3))+"TB"
+
+	return string
+
+def TotalUsage(freeTraffic, outgoingTraffic):
+	result = outgoingTraffic / freeTraffic * 100
+	
+	string = "전체 대비 사용량 %: "+str(round(result,2))+"%"
+	return string
+
+server_1 = GetServer(client_1) # get first server
+server_2 = GetServer(client_2) 
+server_3 = GetServer(client_3) 
+
+def SendAlerts():
+	name_1 = server_1.name
+	traffic_1 = ByteToTB(server_1.outgoing_traffic)
+	usagePercent_1 = TotalUsage(server_1.included_traffic, server_1.outgoing_traffic)
+
+	text = name_1+"\n"+traffic_1+"\n"+usagePercent_1
+	print(text)
+	bot.sendMessage(chat_id=chatID, text=text)
+
+	name_2 = server_2.name
+	traffic_2 = ByteToTB(server_2.outgoing_traffic)
+	usagePercent_2 = TotalUsage(server_2.included_traffic, server_2.outgoing_traffic)
+
+	text = name_2+"\n"+traffic_2+"\n"+usagePercent_2
+	print(text)
+	bot.sendMessage(chat_id=chatID, text=text)
+
+	name_3 = server_3.name
+	traffic_3 = ByteToTB(server_3.outgoing_traffic)
+	usagePercent_3 = TotalUsage(server_3.included_traffic, server_3.outgoing_traffic)
+
+	text = name_3+"\n"+traffic_3+"\n"+usagePercent_3
+	print(text)
+	bot.sendMessage(chat_id=chatID, text=text)
+
+schedule.every().day.at("09:00").do(SendAlerts())
+
+#schedule.every(5).seconds.do(SendAlerts)
+
+while True:
+	schedule.run_pending()
+	time.sleep(1)
