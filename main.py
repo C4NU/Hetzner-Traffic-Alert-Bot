fd783afc869f7a9656ce1be959bed78a71268707
C4NU <canu1832@gmail.com>
2022-08-19 23:42:50 +0900

    [ADD] Add Explanation for code. \n [FIX] Make Class to looking better.

--- main.py
+++ main.py
@@ -1,93 +1,87 @@
-from sched import scheduler
+# Hetzner Cloud API module
 from hcloud import Client
-
 from hcloud.images.domain import Image
 from hcloud.servers.domain import Server
 from hcloud.server_types.domain import ServerType
 from hcloud.volumes.domain import Volume
-
-import urllib.request
+# Schedule module
 import schedule
-import json # json package
+# JSON module
+import json
+# time module
 import time
+# math module 
 import math
+# Python Telegram module
+import telegram
 
-import telegram # python-telegram-module package
+class HetznerTrafficAlertBot:
+	#Initializer
+	def __init__(self):
+		#Open Json file
+		with open("keys.json") as f:
+			keys_data = json.load(f)
+		#Initialize Hetzner Traffic Alert Bot Class
+		self.bot = telegram.Bot(token=keys_data["telegram"]["token"])
+		self.chatID = keys_data["telegram"]["chatID"]
+		#Send Telegram Message "Starting Bot"
+		self.bot.sendMessage(chat_id=self.chatID, text="Starting Bot...")
+		#Initialize client object
+		self.client = Client(token=keys_data["hetzner"]["token"])
+		#Get List of BoundServer
+		self.boundServer = self.client.servers.get_all()
+		#Get First server from Bound Server List
+		#If you have a lot of server from project, use list to get server.
+		self.server = self.boundServer[0] # Get first server from list
 
-# Open Json file - use this section when clone project.
-#with open("keys.json") as f:
-#	keys_data = json.load(f)
+	#Byte To TerraByte Calculator Method
+	def ByteToTB(byteTraffic):
+		#Get Byte Size of Traffic
+		result = byteTraffic
+		#Calculate Byte To TerraByte
+		for i in range(0, 4):
+			result = result / 1024
+		#Make Text for send Message
+		string = "Usage of Traffic: "+str(round(result,3))+"TB"
+		#Return Text
+		return string
 
-with open("my_keys.json") as f:
-	keys_data = json.load(f)
-
-# Initialize Telegram Bot
-bot = telegram.Bot(token=keys_data["telegram"]["token"])
-chatID = keys_data["telegram"]["chatID"]
-
-bot.sendMessage(chat_id=chatID, text="봇 작동 시작")
-# Create a client - remove hashtag
-# client = Client(token=keys_data["hetzner"]["token"])
-
-client_1 = Client(token=keys_data["hetzner"]["canu1832_token"])
-client_2 = Client(token=keys_data["hetzner"]["wakeup_token"])
-client_3 = Client(token=keys_data["hetzner"]["phium_token"])
-
-def GetServer(client):
-	boundServer = client.servers.get_all() # Servers on List
-	server = boundServer[0] # Get first server from list
-
-	return server # return first server
-
-def ByteToTB(byteTraffic):
-	result = byteTraffic
-	for i in range(0, 4):
-		result = result / 1024
+	#Total Usage Percentage Calculator Method
+	def TotalUsage(freeTraffic, outgoingTraffic):
+		#Calculate Total Usage Percentage of Traffics
+		result = outgoingTraffic / freeTraffic * 100
+		#Make Text for send Message
+		string = "Usage %: "+str(round(result,2))+"%"
+		#Return Text
+		return string
 	
-	string = "트래픽 사용량: "+str(round(result,3))+"TB"
+	#Send Alerts to telegram
+	def SendAlerts(self):
+		#Get name of server
+		name = self.server.name
+		#Calculate Current outgoing traffic of your server
+		traffic = self.ByteToTB(self.server.outgoing_traffic)
+		#Calculate usage percentage of your server
+		usagePercent = self.TotalUsage(self.server.included_traffic, self.server.outgoing_traffic)
+		#Make Text message for telegram
+		text = name+"\n"+traffic+"\n"+usagePercent
+		#log
+		print(text)
+		#Send Message
+		self.bot.sendMessage(chat_id=self.chatID, text=text)
 
-	return string
+def main():
+	#Initialize HetznerTrafficAlertBot class
+	hetznerTraffic = HetznerTrafficAlertBot()
 
-def TotalUsage(freeTraffic, outgoingTraffic):
-	result = outgoingTraffic / freeTraffic * 100
+	#Set Schedule, you can adjust. Google python schedule
+	schedule.every().day.at("09:00").do(hetznerTraffic.SendAlerts)
+	schedule.every().day.at("21:00").do(hetznerTraffic.SendAlerts)
 	
-	string = "전체 대비 사용량 %: "+str(round(result,2))+"%"
-	return string
+	#Runnig Bot
+	while True:
+		schedule.run_pending()
+		time.sleep(1)
 
-server_1 = GetServer(client_1) # get first server
-server_2 = GetServer(client_2) 
-server_3 = GetServer(client_3) 
-
-def SendAlerts():
-	name_1 = server_1.name
-	traffic_1 = ByteToTB(server_1.outgoing_traffic)
-	usagePercent_1 = TotalUsage(server_1.included_traffic, server_1.outgoing_traffic)
-
-	text = name_1+"\n"+traffic_1+"\n"+usagePercent_1
-	print(text)
-	bot.sendMessage(chat_id=chatID, text=text)
-
-	name_2 = server_2.name
-	traffic_2 = ByteToTB(server_2.outgoing_traffic)
-	usagePercent_2 = TotalUsage(server_2.included_traffic, server_2.outgoing_traffic)
-
-	text = name_2+"\n"+traffic_2+"\n"+usagePercent_2
-	print(text)
-	bot.sendMessage(chat_id=chatID, text=text)
-
-	name_3 = server_3.name
-	traffic_3 = ByteToTB(server_3.outgoing_traffic)
-	usagePercent_3 = TotalUsage(server_3.included_traffic, server_3.outgoing_traffic)
-
-	text = name_3+"\n"+traffic_3+"\n"+usagePercent_3
-	print(text)
-	bot.sendMessage(chat_id=chatID, text=text)
-
-schedule.every().day.at("09:00").do(SendAlerts)
-schedule.every().day.at("21:00").do(SendAlerts)
-
-#schedule.every(5).seconds.do(SendAlerts)
-
-while True:
-	schedule.run_pending()
-	time.sleep(1)
+if __name__ == '__main__':
+    main()
